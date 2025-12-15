cf7b0d33a11a92ce233af856d02d327ad79e6a2e
Ajaryxx <impressum15@gmail.com>
2025-12-15 20:32:56 +0100

    updatet the add_submodule cmake function

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -2,9 +2,20 @@ function(add_submodule dir)
 
 find_package(Git REQUIRED)
 
-if(NOT EXISTS ${dir}/CMakeLists.txt)
-	execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recrusive ${dir}
+set(FULL_PATH ${CMAKE_SOURCE_DIR}/${dir})
+
+if(NOT EXISTS ${FULL_PATH}/CMakeLists.txt)
+	message("No submodule was found")
+	execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive ${dir}
 	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+
 endif()
+
+if(NOT EXISTS ${FULL_PATH}/CMakeLists.txt)
+message("Couldn't find a CMakeLists in submodule")
+
+else()
 add_subdirectory(${dir})
+endif()
+
 endfunction()
